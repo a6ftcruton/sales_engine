f466686613a19761ade553144ddb7433dbdca40a
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-08-18 20:09:50 -0600

    Add basic tests to invoice

---
+++ test/invoice_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/invoice'
+
+class InvoiceTest < Minitest::Test
+
+  def attributes
+    {
+      id:           8,
+      customer_id:  1,
+      merchant_id:  26,
+      status:       "shipped",
+      created_at:   "2012-03-27 14:53:59 UTC",
+      updated_at:   "2012-03-27 14:53:59 UTC"
+    }
+  end
+
+  def test_it_stores_merchant_id
+    invoice = Invoice.new(attributes)
+    assert_equal 26, invoice.merchant_id
+  end
+
+  def test_it_stores_status
+    invoice = Invoice.new(attributes)
+    assert invoice.respond_to?(:status)
+  end
+
+end
