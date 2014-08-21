cb97e78c30fa2ea88d53cf5cb083729fe4d71550
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-08-21 10:12:11 -0600

    Change _repository to repo in sales_engine tests

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -11,11 +11,11 @@ class SalesEngineTest < Minitest::Test
   def test_it_has_access_to_all_repo_classes_after_startup
     se = SalesEngine.new
     se.startup
-    assert se.respond_to?(:customer_repository)
-    assert se.respond_to?(:invoice_item_repository)
-    assert se.respond_to?(:invoice_repository)
-    assert se.respond_to?(:item_repository)
-    assert se.respond_to?(:merchant_repository)
-    assert se.respond_to?(:transaction_repository)
+    assert se.respond_to?(:customer_repo)
+    assert se.respond_to?(:invoice_item_repo)
+    assert se.respond_to?(:invoice_repo)
+    assert se.respond_to?(:item_repo)
+    assert se.respond_to?(:merchant_repo)
+    assert se.respond_to?(:transaction_repo)
   end
 end
